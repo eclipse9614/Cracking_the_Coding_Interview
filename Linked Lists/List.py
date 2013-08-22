7d55b02061e73dd82163dec7d5b49469a2bc1281
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-08-21 17:13:46 -0700

    Add base class to solutions: Linked List

---
+++ Linked Lists/List.py
@@ -0,0 +1,49 @@
+class Node(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = Node()
+        self.last = self.head
+
+    def _findNodeBeforePos(self, pos):
+        preview = self.head
+        for i in range(pos):
+            preview = preview.next
+            if preview is None:
+                raise IndexError()
+        return preview
+
+    def append(self, value):
+        self.last.next = Node(value)
+        self.last = self.last.next
+
+    def insert(self, pos, val):
+        # find the node before the pos to be inserted
+        preview = self._findNodeBeforePos(pos)
+        newNode = Node(val)
+        newNode.next = preview.next
+        preview.next = newNode
+        # update last pointer
+        if newNode.next is None:
+            self.last = newNode
+
+    def get(self, pos):
+        # find the pos
+        preview = self._findNodeBeforePos(pos)
+        if preview.next:
+            return preview.next.value
+        else:
+            raise IndexError()
+
+    def delete(self, pos):
+        # find the node before the node to be deleted
+        preview = self._findNodeBeforePos(pos)
+        # delete
+        if preview.next:  # if next exist
+            preview.next = preview.next.next
+        else:
+            raise IndexError()
---
+++ Linked Lists/List_Test.py
@@ -0,0 +1,74 @@
+import unittest
+import List
+
+
+class TestList(unittest.TestCase):
+    def setUp(self):
+        self.list = List.LinkedList()
+
+    def testInit(self):
+        self.assertIs(self.list.head.next, None)
+        self.assertIs(self.list.head.value, None)
+        self.assertIs(self.list.head, self.list.last)
+
+    def testAppending(self):
+        curList = self.list
+        curList.append(1)
+        curList.append(2)
+        curList.append(3)
+        self.assertEqual(curList.last.value, 3)
+        self.assertEqual(curList.head.value, None)
+        self.assertEqual(curList.head.next.value, 1)
+        self.assertEqual(curList.head.next.next.value, 2)
+
+    def testInsert(self):
+        curList = self.list
+        # illegal operation
+        self.assertRaises(IndexError, curList.insert, 1, 10)
+        self.assertRaises(IndexError, curList.insert, 2, 10)
+        self.assertRaises(IndexError, curList.insert, 3, 10)
+        # legal operation
+        curList.insert(0, 10)
+        curList.insert(0, 100)
+        curList.insert(0, 1000)
+        self.assertEqual(curList.head.value, None)
+        self.assertEqual(curList.head.next.value, 1000)
+        self.assertEqual(curList.head.next.next.value, 100)
+        self.assertEqual(curList.last.value, 10)
+        curList.insert(1, 999)
+        self.assertEqual(curList.head.value, None)
+        self.assertEqual(curList.head.next.value, 1000)
+        self.assertEqual(curList.head.next.next.value, 999)
+        self.assertEqual(curList.head.next.next.next.value, 100)
+        self.assertEqual(curList.last.value, 10)
+        # insert into last position
+        curList.insert(4, 1)
+        self.assertEqual(curList.last.value, 1)
+        self.assertEqual(curList.head.next.next.next.next.value, 10)
+
+    def testGet(self):
+        curList = self.list
+        # append 100 nodes
+        for x in range(100):
+            curList.append(x ** 2)
+        for x in range(100):
+            self.assertEqual(curList.get(x), x ** 2)
+        for x in range(100, 105):
+            self.assertRaises(IndexError, curList.get, x)
+
+    def testDelete(self):
+        curList = self.list
+        # illegal
+        for i in range(10):
+            self.assertRaises(IndexError, curList.delete, i)
+        # append 100 nodes after head
+        for x in range(100):
+            curList.append(x)
+        res = [i for i in range(100)]
+        # delete 99, 50, 49, 38, 20, 18, 11, 5, 0
+        deleted = [99, 50, 49, 38, 20, 18, 11, 5, 0]
+        for i in deleted:
+            curList.delete(i)
+            del res[i]
+        for pos, element in enumerate(res):
+            self.assertEqual(curList.get(pos), element)
